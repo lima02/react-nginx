abe62391985de2560318f83a8177115939096c4c
Lima, Miguel <limamigu@amazon.com>
2020-08-12 20:56:27 +1000

    removed config.js from repository

--- config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    vpcId: 'vpc-5bb8883c',
-};
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiY29uZmlnLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsa0JBQWU7SUFDWCxLQUFLLEVBQUUsY0FBYztDQUN4QixDQUFBIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQge1xuICAgIHZwY0lkOiAndnBjLTViYjg4ODNjJyxcbn0iXX0=
